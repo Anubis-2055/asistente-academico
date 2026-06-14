538541b4e7fb729ddb7e51fc6fa1ad51efb3c514
Anubis <michafajardo444@gmail.com>
2026-06-13 21:31:43 -0500

    feat: initial project structure - Spring Boot setup

---
+++ src/main/java/com/uteq/asistente_academico/AsistenteAcademicoApplication.java
@@ -0,0 +1,13 @@
+package com.uteq.asistente_academico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsistenteAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsistenteAcademicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uteq/asistente_academico/AsistenteAcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uteq.asistente_academico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistenteAcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
